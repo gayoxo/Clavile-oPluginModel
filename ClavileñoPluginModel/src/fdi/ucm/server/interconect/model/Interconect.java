97070d8cd6de62e124c854a9ffc6766ae6037111
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2017-10-18 13:34:38 +0200

    Me Bajo A Comer

--- ClavileñoPluginModel/src/fdi/ucm/server/interconect/model/Interconect.java
+++ ClavileñoPluginModel/src/fdi/ucm/server/interconect/model/Interconect.java
@@ -18,9 +18,18 @@ public class Interconect implements Serializable {
 	public static final String EDITOR = "editor";
 	
 	//NECESARIAS EN LOS ENVIOS PARA PODER EDITAR
+	
 	public static final String POSTURL="posturl";
 	public static final String PASSID = "passid";
 	public static final String PROTOCOL = "protocol";
+	public static final String MULTI = "multi";
+	public static final String EXTRADATA = "extradata";
+	public static final String DATAGETURL = "datageturl";
+	public static final String VISUALDATAGETURL = "visualdatageturl";
+	public static final String POSITION = "position";
+	public static final String DOCUMENTID = "documentpartid";
+	public static final String EDITORNAME = "editorname";
+	
 	
 	
 	private String Name;
